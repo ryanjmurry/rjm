ff3ff22c1b751675cbb161aed48fb26f54ea54a1
Ryan Murry <ryanjmurry@gmail.com>
2018-11-06 12:57:42 -0800

    create component

---
+++ src/features/projects/ProjectsPage/ProjectsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProjectsPage() {
+  return (
+    <div>
+      <h1>Projects Page Works!</h1>
+    </div>
+  )
+}
+
+export default ProjectsPage
---
+++ src/features/skills/SkillsPage/SkillsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SkillsPage() {
+  return (
+    <div>
+      <h1>Skills Page Works!</h1>
+    </div>
+  )
+}
+
+export default SkillsPage
