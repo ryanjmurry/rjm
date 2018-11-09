ff1f17bb51d48b4f5814c15d39110bbfe8976e60
Ryan Murry <ryanjmurry@gmail.com>
2018-11-09 14:07:52 -0800

    add fontAwesome library

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -5,6 +5,11 @@ import HomePage from '../../features/home/HomePage/HomePage';
 import ProfilePage from '../../features/profile/ProfilePage/ProfilePage';
 import ProjectsPage from '../../features/projects/ProjectsPage/ProjectsPage';
 import SkillsPage from '../../features/skills/SkillsPage/SkillsPage';
+import { library } from '@fortawesome/fontawesome-svg-core'
+// import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+ 
+library.add(fas)
 
 class App extends Component {
   render() {
@@ -16,9 +21,9 @@ class App extends Component {
         <div>
           <Switch>
             <Route exact path="/" component={HomePage} />
-            <Route path="/profile" component={ProfilePage} />
+            {/* <Route path="/profile" component={ProfilePage} />
             <Route path="/projects" component={ProjectsPage} />
-            <Route path="/skills" component={SkillsPage} />
+            <Route path="/skills" component={SkillsPage} /> */}
           </Switch>
         </div>
       </div>
