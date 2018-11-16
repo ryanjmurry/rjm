72e8dc798c49f8fe91d458798fdf10f38e21ab9b
Ryan Murry <ryanjmurry@gmail.com>
2018-11-16 00:58:05 -0800

    change layout, add container styles, page transitions

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,34 +1,94 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import NavBar from '../../features/nav/NavBar/NavBar';
 import HomePage from '../../features/home/HomePage/HomePage';
 import ProfilePage from '../../features/profile/ProfilePage/ProfilePage';
 import ProjectsPage from '../../features/projects/ProjectsPage/ProjectsPage';
 import SkillsPage from '../../features/skills/SkillsPage/SkillsPage';
-import { library } from '@fortawesome/fontawesome-svg-core'
-// import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
- 
-library.add(fas)
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <div>
-          <NavBar />
-        </div>
-        <div>
-          <Switch>
-            <Route exact path="/" component={HomePage} />
-            {/* <Route path="/profile" component={ProfilePage} />
-            <Route path="/projects" component={ProjectsPage} />
-            <Route path="/skills" component={SkillsPage} /> */}
-          </Switch>
-        </div>
-      </div>
-    );
+library.add(fas, fab);
+
+const Wrapper = styled.div`
+  .fade-enter {
+    opacity: 0.01;
   }
+
+  .fade-enter.fade-enter-active {
+    opacity: 1;
+    transition: opacity 500ms ease-in;
+  }
+
+  .fade-exit {
+    opacity: 1;
+  }
+
+  .fade-exit.fade-exit-active {
+    opacity: 0.01;
+    transition: opacity 500ms ease-in;
+  }
+
+  div.transition-group {
+    position: relative;
+  }
+
+  span.route-section {
+    position: absolute;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 100%;
+  }
+
+  .container {
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+  @media (min-width: 768px) {
+    .container {
+      width: 750px;
+    }
+  }
+  @media (min-width: 992px) {
+    .container {
+      width: 970px;
+    }
+  }
+  @media (min-width: 1200px) {
+    .container {
+      width: 1170px;
+    }
+  }
+`;
+
+function App({ location }) {
+  return (
+    <Wrapper>
+      <div>
+        <NavBar />
+      </div>
+      <div className="container">
+        <TransitionGroup className="transition-group">
+          <CSSTransition key={location.key} classNames="fade" timeout={500}>
+            <span className="route-section">
+              <Switch location={location}>
+                  <Route exact path="/" component={HomePage} />
+                  <Route path="/profile" component={ProfilePage} />
+                  <Route path="/projects" component={ProjectsPage} />
+                  <Route path="/skills" component={SkillsPage} />
+              </Switch>
+            </span>
+          </CSSTransition>
+        </TransitionGroup>
+      </div>
+    </Wrapper>
+  );
 }
 
-export default App;
+export default withRouter(App);
