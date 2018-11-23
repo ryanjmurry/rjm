23ef97460e3d9c967f9e47b5c8a1bd5dc46e5724
Ryan Murry <ryanjmurry@gmail.com>
2018-11-23 09:27:25 -0800

    create constants file with menu constants

---
+++ src/app/common/constants.js
@@ -0,0 +1,49 @@
+export const navigationalMenuItems = {
+  home: {
+    iconName: 'home',
+    color: '#f9f871',
+    displayName: 'home'
+  },
+  projects: {
+    iconName: 'pencil-ruler',
+    color: '#ffb75c',
+    displayName: 'projects'
+  },
+  profile: {
+    iconName: 'user-tie',
+    color: '#f77c69',
+    displayName: 'profile'
+  },
+  skills: {
+    iconName: 'code',
+    color: '#c7517b',
+    displayName: 'skills'
+  }
+};
+
+export const socialMenuItems = {
+  email: {
+    iconName: 'envelope',
+    color: '#ffb75c',
+    // color: '#ea4335',
+    link: 'mailto:ryanjmurry@gmail.com'
+  },
+  twitter: {
+    iconName: ['fab', 'twitter'],
+    color: '#ffb75c',
+    // color: '#1da1f2',
+    link: 'https://www.twitter.com/ryanjmurry'
+  },
+  gitHub: {
+    iconName: ['fab', 'github-alt'],
+    color: '#ffb75c',
+    // color: '#2fbf50',
+    link: 'https://github.com/ryanjmurry'
+  },
+  linkedIn: {
+    iconName: ['fab', 'linkedin-in'],
+    // color: '#007bb5',
+    color: '#ffb75c',
+    link: 'https://www.linkedin.com/in/ryanjmurry/'
+  }
+};
