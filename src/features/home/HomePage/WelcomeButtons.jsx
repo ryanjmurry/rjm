cf7350dec715fa7dedfc48561965cf887cc84137
Ryan Murry <ryanjmurry@gmail.com>
2018-11-25 08:15:52 -0800

    add responsiveness

--- src/features/home/HomePage/WelcomeButtons.jsx
+++ src/features/home/HomePage/WelcomeButtons.jsx
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../nav/Button/Button';
 
+const mobileStyles = {
+  height: '150px',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-evenly'
+}
+
+const desktopStyles = {};
+
 class WelcomeButtons extends Component {
   render() {
+    const { screenWidth } = this.props;
     return (
-      <div>
+      <div style={screenWidth <= 560 ? mobileStyles : desktopStyles}>
         <Link to="/projects">
           <Button buttonText="explore work" />
         </Link>
