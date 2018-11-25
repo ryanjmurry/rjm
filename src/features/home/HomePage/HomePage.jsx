508d5e29b56eff4e9b5ed24cdf9eb5f629b1ffde
Ryan Murry <ryanjmurry@gmail.com>
2018-11-25 08:14:23 -0800

    media query with screenWidth passed down

--- src/features/home/HomePage/HomePage.jsx
+++ src/features/home/HomePage/HomePage.jsx
@@ -10,33 +10,33 @@ const HomePageContainer = styled.div`
   position: relative;
   top: 200px;
 
-  // @media (max-width: 768px) {
-  //   top: 0px;
-  //   display: -webkit-box;
-  //   display: -webkit-flex;
-  //   display: -ms-flexbox;
-  //   display: flex;
-  //   -webkit-box-pack: center;
-  //   -webkit-justify-content: center;
-  //   -ms-flex-pack: center;
-  //   justify-content: center;
-  //   align-items: center;
-  //   flex-direction: column;
-  //   text-align: center;
-  //   top: 100px;
-  //   }
-  // }
+  @media (max-width: 768px) {
+    top: 0px;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: center;
+    -webkit-justify-content: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+    top: 100px;
+    }
+  }
 `;
 
 //------ styles end ------//
 
-function HomePage() {
+const HomePage = ({screenWidth}) => {
   return (
     <div>
       <Snow />
       <HomePageContainer>
-        <Welcome />
-        <WelcomeButtons />
+        <Welcome screenWidth={screenWidth}/>
+        <WelcomeButtons screenWidth={screenWidth}/>
       </HomePageContainer>
     </div>
   );
