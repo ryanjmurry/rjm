0323860e8a6334b76df1ad06f132c9fb87ebd861
Ryan Murry <ryanjmurry@gmail.com>
2018-11-28 17:02:49 +0000

    remove className

--- src/features/skills/SkillsPage/SkillsPage.jsx
+++ src/features/skills/SkillsPage/SkillsPage.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function SkillsPage() {
   return (
-    <div style={{paddingLeft: '200px'}}>
-      <h1>Skills Page Works!</h1>
+    <div>
+      <h1>Skills</h1>
     </div>
   )
 }
