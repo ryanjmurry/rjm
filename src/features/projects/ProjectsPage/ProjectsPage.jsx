03c95780a2d3f9a5e77aa4c5ca11f4737a920f04
Ryan Murry <ryanjmurry@gmail.com>
2018-11-28 20:14:46 +0000

    add styled component and map through projects object

--- src/features/projects/ProjectsPage/ProjectsPage.jsx
+++ src/features/projects/ProjectsPage/ProjectsPage.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Project from './Project';
+import { projects } from '../../../app/common/constants';
+
+const Wrapper = styled.div`
+  h1 {
+    font-size: 3.5em;
+  }
+`;
 
 function ProjectsPage() {
   return (
-    <div style={{paddingLeft: '200px'}}>
-      <h1>Projects Page Works!</h1>
-    </div>
-  )
+    <Wrapper>
+      <h1>Projects</h1>
+      {Object.keys(projects).map(key => (
+        <Project
+          key={key}
+          projectName={projects[key].displayName}
+          teamProject={projects[key].team}
+          gitHubUrl={projects[key].gitHub}
+          hosted={projects[key].liveSite.hosted}
+          liveSiteUrl={projects[key].liveSite.url}
+          technologies={projects[key].technologies}
+        />
+      ))}
+    </Wrapper>
+  );
 }
 
-export default ProjectsPage
+export default ProjectsPage;
