09a206115c1a04750f5d46e4b38c79b5b3315f6f
Ryan Murry <ryanjmurry@gmail.com>
2018-11-29 01:11:51 +0000

    finish mobile page styles

--- src/features/projects/ProjectsPage/ProjectsPage.jsx
+++ src/features/projects/ProjectsPage/ProjectsPage.jsx
@@ -3,14 +3,22 @@ import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Project from './Project';
 import { projects } from '../../../app/common/constants';
+import Button from '../../nav/Button/Button';
 
 const Wrapper = styled.div`
   h1 {
+    margin: 40px 0 0 0;
     font-size: 3.5em;
+    padding-left: 40px;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+  }
+
+  .page-button {
+    margin: 40px 0px 40px 35px;
   }
 `;
 
-function ProjectsPage() {
+function ProjectsPage({ screenWidth }) {
   return (
     <Wrapper>
       <h1>Projects</h1>
@@ -18,13 +26,18 @@ function ProjectsPage() {
         <Project
           key={key}
           projectName={projects[key].displayName}
+          description={projects[key].description}
           teamProject={projects[key].team}
           gitHubUrl={projects[key].gitHub}
           hosted={projects[key].liveSite.hosted}
           liveSiteUrl={projects[key].liveSite.url}
           technologies={projects[key].technologies}
+          screenWidth={screenWidth}
         />
       ))}
+      <div className='page-button'>
+        <Button buttonText="explore my skills" />
+      </div>
     </Wrapper>
   );
 }
