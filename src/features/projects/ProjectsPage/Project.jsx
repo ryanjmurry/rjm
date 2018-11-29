9b73b01fe0e9434b3580823c67f11e9d793e511e
Ryan Murry <ryanjmurry@gmail.com>
2018-11-29 01:11:41 +0000

    finish mobile page styles

--- src/features/projects/ProjectsPage/Project.jsx
+++ src/features/projects/ProjectsPage/Project.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import onClickOutside from 'react-onclickoutside';
 
 const ProjectBox = styled.div`
-  border-top: 1px solid rgba(255, 255, 255, 0.5);
+  border-bottom: 1px solid rgba(255, 255, 255, 0.5);
   width: 100%;
   position: relative;
   left: 0;
@@ -15,27 +15,29 @@ const InactiveBox = styled.div`
   display: flex;
   align-items: center;
   height: 110px;
-  justify-content: space-evenly;
+  justify-content: space-between;
   color: #fff;
-  transition: 0.5s;
-  margin: 0px 10px;
+  margin: 0px 30px;
 `;
 
 const ProjectSection = styled.div`
   display: flex;
   flex-direction: column;
-  width: 155px;
+  width: 200px;
   font-family: 'Laila', serif;
   font-weight: 600;
+  font-size: 1.2em;
 `;
 
 const TechnologySection = styled.div`
   display: flex;
   flex-wrap: wrap;
-  width: 200px;
-  justify-content: center;
+  width: 125px;
+  // justify-content: center;
   font-family: 'Montserrat', sans-serif;
   font-weight: 400;
+  font-size: 1em;
+  margin-left: 15px;
 
   span {
     margin-right: 10px;
@@ -44,6 +46,8 @@ const TechnologySection = styled.div`
 
 const ActiveBox = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: space-around;
   transition: 0.5s;
   height: 110px;
@@ -51,6 +55,7 @@ const ActiveBox = styled.div`
   h3 {
     color: #1c306d;
     font-weight: 400;
+    margin-top: -5px;
   }
 
   a {
@@ -58,6 +63,20 @@ const ActiveBox = styled.div`
     color: #1c306d;
     font-weight: 400;
   }
+
+  p {
+    color: #1c306d;
+  }
+`;
+
+const TeamSoloTag = styled.div`
+  position: absolute;
+  top: 0;
+  right: 15px;
+  font-family: 'Laila', serif;
+  color: #ffb75c;
+  font-weight: 600;
+  font-size: 0.9em;
 `;
 
 class Project extends Component {
@@ -86,28 +105,43 @@ class Project extends Component {
 
   render() {
     const { boxBackgroundColor, projectActive } = this.state;
-    const { projectName, technologies, teamProject, hosted, gitHubUrl, liveSiteUrl } = this.props;
+    const {
+      projectName,
+      description,
+      technologies,
+      teamProject,
+      hosted,
+      gitHubUrl,
+      liveSiteUrl,
+      screenWidth
+    } = this.props;
+    const teamOrSolo = teamProject ? 'team' : 'solo';
+    const techDisplay =
+      screenWidth < 768 ? (
+        <span>{technologies[0]}</span>
+      ) : (
+        technologies.map((tech, index) => <span key={index}>{tech}</span>)
+      );
+
     return (
       <ProjectBox onClick={this.projectClick} style={{ backgroundColor: boxBackgroundColor }}>
         {!projectActive && (
           <InactiveBox>
+            <TeamSoloTag>{teamOrSolo}</TeamSoloTag>
             <ProjectSection>
               <span>{projectName}</span>
             </ProjectSection>
-            <TechnologySection>
-              {technologies.map((tech, index) => (
-                <span key={index}>{tech}</span>
-              ))}
-            </TechnologySection>
+            <TechnologySection>{techDisplay}</TechnologySection>
             <FontAwesomeIcon icon="chevron-right" />
           </InactiveBox>
         )}
         {projectActive && (
           <ActiveBox>
+            <p>{description}</p>
             {hosted ? (
               <h3>
                 <a href={gitHubUrl}>GitHub</a>
-                <span style={{ margin: '0px 10px' }}>|</span> <a href={liveSiteUrl}>Live Site</a>
+                <span style={{ marginLeft: '10px' }}>|</span> <a href={liveSiteUrl}>Live Site</a>
               </h3>
             ) : (
               <h3>
