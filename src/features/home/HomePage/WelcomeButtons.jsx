04377084a026592fc4997ee5f14aac6bf6ef1518
Ryan Murry <ryanjmurry@gmail.com>
2018-11-30 01:17:31 +0000

    refactor into functional component

--- src/features/home/HomePage/WelcomeButtons.jsx
+++ src/features/home/HomePage/WelcomeButtons.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../nav/Button/Button';
 
@@ -11,11 +11,9 @@ const mobileStyles = {
 
 const desktopStyles = {};
 
-class WelcomeButtons extends Component {
-  render() {
-    const { screenWidth } = this.props;
-    return (
-      <div style={screenWidth <= 560 ? mobileStyles : desktopStyles}>
+const WelcomeButtons = ({screenWidth}) => {
+  return (
+    <div style={screenWidth <= 560 ? mobileStyles : desktopStyles}>
         <Link to="/projects">
           <Button buttonText="explore work" />
         </Link>
@@ -23,8 +21,8 @@ class WelcomeButtons extends Component {
           <Button buttonText="view profile" />
         </Link>
       </div>
-    );
-  }
+  )
 }
 
 export default WelcomeButtons;
+
