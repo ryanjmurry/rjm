cd55f8dba87a2dd2f74f0df6ebaab3e36fd0253c
Ryan Murry <ryanjmurry@gmail.com>
2018-12-04 08:24:21 -0800

    add divider on mobile screens

--- src/features/home/HomePage/Welcome.jsx
+++ src/features/home/HomePage/Welcome.jsx
@@ -5,17 +5,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 //------ styles start ------//
 
 const Wrapper = styled.div`
-h1 {
-  line-height: 1;
-  margin-bottom: -30px;
-}
+  h1 {
+    line-height: 1;
+    margin-bottom: -30px;
+  }
 
-h3 {
-  display: flex;
-  align-items : center;
-}
+  .large-screen {
+    display: flex;
+    align-items : center;
+  }
 
+  hr {
+    margin-top: -20px;
+    color: rgba(255, 255, 255, 0.4);
+    border: 1px solid rgba(255, 255, 255, 0.6);
+    width: 85%;
+  }
 
+  hr + h3 {
+    margin-top: 0px;
+  }
+  
 `;
 
 //------ styles end ------//
@@ -26,17 +36,18 @@ const Welcome = ({ screenWidth }) => {
       <h1>Ryan Murry</h1>
 
       {screenWidth > 560 && (
-       <h3>
-       Software Developer
-       <FontAwesomeIcon icon="circle" style={{fontSize: '0.25em', margin: '0px 20px'}}/>
-       Educator
-     </h3>
+        <h3 className='large-screen'>
+          Software Developer
+          <FontAwesomeIcon icon="circle" style={{ fontSize: '0.25em', margin: '0px 20px' }} />
+          Educator
+        </h3>
       )}
       {screenWidth < 560 && (
-        <h3>
-        Software Developer <br />
-        Educator
-      </h3>
+        <div className='small-screen'>
+          <h3>Software Developer</h3>
+          <hr />
+          <h3>Educator</h3>
+        </div>
       )}
     </Wrapper>
   );
