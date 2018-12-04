d65b39c87f40887dbb30c698f0052265d66c29ab
Ryan Murry <ryanjmurry@gmail.com>
2018-12-03 19:37:40 -0800

    add text, styles, and responsiveness

--- src/features/profile/ProfilePage/ProfilePage.jsx
+++ src/features/profile/ProfilePage/ProfilePage.jsx
@@ -1,39 +1,122 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import ProfileButtons from './ProfileButtons';
 
-function ProfilePage() {
-  return (
-    <div>
-      <h1>Profile</h1>
-      <div>
-        <p>
-          Hi. I'm a software developer and former high school chemistry teacher currently living in Portland, OR. 
-        </p>
-        <p>
-          Nullam sit amet scelerisque elit, vitae consequat dui. Donec efficitur, ex mattis
-          dignissim euismod, lectus orci sagittis orci, eu cursus eros enim placerat risus. Maecenas
-          et augue venenatis, porttitor urna placerat, vulputate eros.
-        </p>
-        <p>
-          Pellentesque nec augue sed elit luctus ornare sed sed tellus. Orci varius natoque
-          penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam pretium quam in ex
-          ornare luctus. Nulla euismod congue molestie. Donec ac venenatis nibh. Vestibulum nunc
-          lorem, accumsan id orci sit amet, facilisis commodo ligula. Quisque at elit ante.
-        </p>
-        <p>
-          Nullam iaculis dignissim magna, id vestibulum mi molestie vitae. Suspendisse non
-          ullamcorper nisi. Vestibulum dignissim sed mi sed vestibulum. Donec sed nibh eu ligula
-          pellentesque ornare vitae in orci.
-        </p>
-        <p>
-          Nam fermentum justo sapien, sed auctor tellus consequat a. Suspendisse non felis neque.
-        </p>
-        <p>
-          Etiam a varius turpis. Donec ex augue, faucibus vel consequat eu, facilisis a ipsum. Sed
-          vitae quam quis ex vehicula viverra.
-        </p>
-      </div>
-    </div>
+const Wrapper = styled.div`
+  h1 {
+    font-size: 3.5em;
+    padding-left: 40px;
+  }
+
+  p {
+    font-weight: 300;
+    font-size: 1.35em;
+  }
+
+  .page-button {
+    margin: 40px 0px 50px -50px;
+  }
+
+  .page-button p {
+    font-weight: 600;
+  }
+
+  .project-link {
+    text-decoration: none;
+    color: #fff;
+    font-weight: bold;
+  }
+
+  @media (max-width: 768px) {
+    .page-button {
+      margin: 40px 0px 50px 35px;
+    }
+  }
+
+  @media (max-width: 495px) {
+    .page-button {
+      margin: 40px 0px 50px 0px;
+    }
+  }
+`;
+
+const TextBox = styled.div`
+  @media (max-width: 768px) {
+      margin: 0px 40px;
+  }
+  margin-left: -40px;
+  margin-right: 40px;
+  margin-top: -10px;
+  max-width: 750px;
+`;
+
+const ProfilePage = ({ screenWidth }) => {
+  const mobileHeader = <h1 style={{ margin: '40px 0 0 0' }}>About Ryan</h1>;
+  const desktopHeader = (
+    <h1 style={{ padding: '80px 0 0', marginLeft: '-40px', marginBottom: '0px', width: '75vw' }}>
+      About Ryan
+    </h1>
   );
-}
+  return (
+    <Wrapper>
+      {screenWidth > 768 ? desktopHeader : mobileHeader}
+      <TextBox>
+        <p>
+          Hi! I'm a dynamic and detail-oriented software developer with an aptitude for visualizing
+          and delivering elegant, user-friendly solutions effectively and efficiently.
+        </p>
+        <p>
+          Prior to becoming a software developer, I worked as an analytical chemist specializing in
+          environmental testing and solution analysis and have spent the past four years as a high
+          school chemistry teacher and varsity football coach.
+        </p>
+        <p>
+          I've recently graduated from Epicodus, a 20-week web development school based in Portland,
+          OR. During that time, I've had the opportunity to immerse myself in popular languages,
+          libraries, and frameworks such as C#, ES6, ASP.NET Core, Angular 6, and React.
+        </p>
+        <p>
+          I enjoy both front-end and back-end development. I take pride in designing client-side
+          solutions that are intuitive and easy to interact with, and also am very interested in
+          database design, management, and architecture.
+        </p>
+        <p>
+          Browse some of my{' '}
+          <Link className="project-link" to="/projects">
+            work
+          </Link>
+          , view my{' '}
+          <Link className="project-link" to="/skills">
+            skills
+          </Link>
+          , or{' '}
+          <a className="project-link" href="mailto:ryanjmurry@gmail.com">
+            shoot me an email
+          </a>
+          !
+        </p>
+        <p>
+          You can also find me on{' '}
+          <a className="project-link" href="https://www.linkedin.com/in/ryanjmurry/">
+            LinkedIn
+          </a>
+          ,{' '}
+          <a className="project-link" href="https://github.com/ryanjmurry">
+            GitHub
+          </a>
+          , and{' '}
+          <a className="project-link" href="https://www.twitter.com/ryanjmurry">
+            Twitter
+          </a>
+          .
+        </p>
+      </TextBox>
+      <div className="page-button">
+        <ProfileButtons screenWidth={screenWidth} />
+      </div>
+    </Wrapper>
+  );
+};
 
 export default ProfilePage;
